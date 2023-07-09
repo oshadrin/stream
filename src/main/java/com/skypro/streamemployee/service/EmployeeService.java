101cac1cea144657d09f6c13528cbe48593d215d
olegshadrin <sholegemail@gmail.com>
2023-07-09 12:55:23 +0300

    complete libraries

--- src/main/java/com/skypro/streamemployee/service/EmployeeService.java
+++ src/main/java/com/skypro/streamemployee/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.skypro.streamemployee.service;
 
 
+import com.skypro.streamemployee.exception.EmployeeInvalidDataException;
 import com.skypro.streamemployee.model.Employee;
 import com.skypro.streamemployee.exception.EmployeeAlreadyAddedException;
 import com.skypro.streamemployee.exception.EmployeeArrayIsFullException;
 import com.skypro.streamemployee.exception.EmployeeNotFoundException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -22,12 +24,16 @@ public class EmployeeService {
     }
 
     public Employee addEmployee(Employee employee) {
+        if (!StringUtils.isAlpha(employee.getFirstName()) || !StringUtils.isAlpha(employee.getLastName())){
+        throw new EmployeeInvalidDataException();
+        }
         if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException("Сотрудник уже существует");
         }
         if (employees.size() >= MAX_SIZE) {
             throw new EmployeeArrayIsFullException("Коллекция сотрудников переполнена");
         }
+        correctCase(employee);
         employees.put(createKey(employee), employee);
         return employee;
     }
@@ -55,6 +61,10 @@ public class EmployeeService {
 
     private static String createKey(String firstName, String lastName) {
         return (firstName + lastName).toLowerCase();
+    }
+    private static void correctCase(Employee employee){
+        employee.setFirstName(StringUtils.capitalize(employee.getFirstName().toLowerCase()));
+        employee.setLastName(StringUtils.capitalize(employee.getLastName().toLowerCase()));
     }
 }
 
